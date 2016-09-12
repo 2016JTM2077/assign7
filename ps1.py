02526db2e3273c71566b48352d5ea16e9d0ab184
2016JTM2077 <saxena.karan8494@gmail.com>
2016-09-12 14:42:47 +0530

    1st

--- ps1.py
+++ ps1.py
@@ -1,3 +1,19 @@
-###### this is the first .py file ###########
+#!/usr/bin/python
 
-####### write your code here ##########
+inp=raw_input()
+inp2=inp.split( );
+l=len(inp)
+
+default_d={}
+
+
+#--------------------------------------DIctionary--------------------------------------------
+for i in inp2:
+	p=inp2.count(i)
+	default_d.update({i: p})
+
+
+#-------------------------------------Generating top 3 frequent words------------------------
+out= sorted(default_d.iteritems(), key=lambda x:-x[1])[:3]
+for field in out :
+	print "{0}: {1}".format(*field)
--- ps2.py
+++ ps2.py
@@ -1,3 +1,15 @@
-###### this is the second .py file ###########
+#!/usr/bin/python
 
-####### write your code here ##########
+import random
+u=0   #variable for number of users
+count=0     #variable for number of users in the unit circle
+for u in range(1,101):
+	(X,Y)=(random.random()*2- 1, random.random()*2-1)
+	u+=1
+	
+	if((X**2+Y**2)<=1):
+		count+=1
+
+perc=(float(count)/100)*100   #percentage of users
+
+print "Percentage of users in the unit circle around the MSC is %d" %perc
